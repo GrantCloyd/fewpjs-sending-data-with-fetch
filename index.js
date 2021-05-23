562d9e3123ce2941055d8f0274e084af692fb474
Grant Cloyd <dgcloyd@gmail.com>
2021-05-23 15:09:28 -0500

    commit tree 980f6fe4a3d0e3a509b080f448bd0ccdadafff44

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Add your code here
+
+// const configObject = {
+//    headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json",
+//    },
+//    body: JSON.stringify({
+//       dogName: "SeeSaw Joe",
+//       dogBreed: "Mastiff",
+//    }),
+// }
+
+// fetch("http://localhost:3000/dogs", configObject)
+//    .then(response => response.json())
+//    .then(json => console.log(json))
+//    .catch(error => {
+//       console.warn(`Oh cat fuckery batman - it's not going to make it. It just gave us this:`)
+//       console.log(error)
+//    })
+
+const appendToDOM = data => {
+   const p = document.createElement("p")
+   p.textContent = data.id
+   document.body.append(p)
+}
+const appendFailToDOM = data => {
+   const p = document.createElement("p")
+   p.textContent = data.message
+   document.body.append(p)
+}
+
+function submitData(userName, userEmail) {
+   let userObj = {
+      method: "POST",
+      headers: {
+         "Content-Type": "application/json",
+         Accept: "application/json",
+      },
+      body: JSON.stringify({
+         name: userName,
+         email: userEmail,
+      }),
+   }
+
+   return fetch("http://localhost:3000/users", userObj)
+      .then(response => response.json())
+      .then(appendToDOM)
+      .catch(appendFailToDOM)
+}
